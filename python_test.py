a25d47748b5f228d4d469163417e6aaa02a0fc06
baraku <baraku33@gmail.com>
2025-11-13 11:10:05 +0300

    a simple calculator

--- python_test.py
+++ python_test.py
@@ -1,3 +1,15 @@
-num1 = input('enter number: ')
-
-print(num1)
+num1 = int(input('Please enter your first number: '))
+sign = input('Enter the operator sign: ')
+num2 = int(input('Please enter your second number: '))
+if sign == '+':
+    result = num1 + num2
+    print('The result is: '+ str(result))
+elif sign == '-':
+    result = num1 - num2
+    print('The result is: '+ str(result))
+elif sign == '*':
+    result = num1 * num2
+    print('The result is: '+ str(result))
+elif sign == '/':
+    result = num1 / num2
+    print('The result is: '+ str(result))    
